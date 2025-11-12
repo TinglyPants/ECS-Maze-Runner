2f390087436c590bd5a1bddb5cd962ee4ad90f2b
TinglyPants <chalkytangfastics@gmail.com>
2025-11-12 20:59:41 +0000

    Get walls - untested

--- src/maze.py
+++ src/maze.py
@@ -52,3 +52,60 @@ def create_maze(width: int = 5, height: int = 5) -> list[list[list[bool]]]:
                 generated_maze[i][j][int(Direction.NORTH)] = True # Set north wall
 
     return generated_maze
+
+
+def add_horizontal_wall(maze: list[list[list[bool]]], x_coordinate: int, horizontal_line: int) -> list[list[list[bool]]]:
+    pass
+
+def add_vertical_wall(maze: list[list[list[bool]]], y_coordinate: int, vertical_line: int) -> list[list[list[bool]]]:
+    pass
+
+def get_dimensions(maze: list[list[list[bool]]]) -> tuple[int, int]:
+    pass
+
+def get_walls(maze: list[list[list[bool]]], x_coordinate: int, y_coordinate: int) -> tuple[bool, bool, bool, bool]:
+    """Return the cell at a given position in the maze.
+
+    Each cell in the maze is represented by a list with 4 boolean values to indicate the presence of walls.
+    Indexing the cell should be done by casting a `Direction` to an int.
+    A value of `True` implies a wall is present.
+
+    Parameters
+    ----------
+    maze : list[list[list[bool]]]
+        The maze to search for cells.
+    x_coordinate : int
+        The x coordinate of the cell.
+    y_coordinate : int
+        The y coordinate of the cell.
+
+    Returns
+    -------
+    tuple[bool, bool, bool, bool]
+        The cell at a given position in the maze.
+    """
+    if not (isinstance(maze, list) and isinstance(maze[0], list)):
+        raise TypeError(f"maze must be list[list]!")
+    if not isinstance(x_coordinate, int):
+        raise TypeError(f"x_coordinate must be int, got {type(x_coordinate).__name__}")
+    if not isinstance(y_coordinate, int):
+        raise TypeError(f"y_coordinate must be int, got {type(y_coordinate).__name__}")
+    if x_coordinate < 0:
+        raise ValueError(f"x_coordinate must be greater than or equal to 0, got {x_coordinate}")
+    if y_coordinate < 0:
+        raise ValueError(f"y_coordinate must be greater than or equal to 0, got {y_coordinate}")
+
+    width = len(maze)
+    height = len(maze[0])
+
+    if x_coordinate >= width:
+        raise ValueError(f"x_coordinate must be less than width ({width}), got {x_coordinate}")
+    if y_coordinate >= height:
+        raise ValueError(f"y_coordinate must be less than height ({height}), got {y_coordinate}")
+
+    cell = maze[x_coordinate][y_coordinate]
+
+    if not (isinstance(cell, list) and isinstance(cell[0], bool)):
+        raise TypeError(f"maze cell must be list[bool]!")
+
+    return cell[0], cell[1], cell[2], cell[3]
