a767147cd2ed9c5761e5116927fabb0fd213a8c3
TinglyPants <chalkytangfastics@gmail.com>
2025-11-16 03:32:28 +0000

    Added tests, all passing

--- tests/test_maze_runner.py
+++ tests/test_maze_runner.py
@@ -8,7 +8,7 @@ from maze_runner import *
 
 
 def test_shortest_path():
-    """Unit test shortest path."""
+    """Unit test shortest_path."""
     maze = create_maze(11, 5)
     maze = add_horizontal_wall(maze, 0, 1)
     maze = add_vertical_wall(maze, 1, 1)
@@ -20,3 +20,42 @@ def test_shortest_path():
         x, y, a = location
         assert (x, y) not in prefix, f"{location} is repeated"
         prefix.append((x, y))
+
+
+def test_maze_reader():
+    """Unit test maze_reader."""
+    tiny_maze = maze_reader("src/tiny_maze.mz")
+    assert get_dimensions(tiny_maze) == (1, 1)
+    assert get_walls(tiny_maze, 0, 0) == (True, True, True, True)
+
+    medium_empty_maze = maze_reader("src/medium_empty_maze.mz")
+    assert get_dimensions(medium_empty_maze) == (3, 3)
+    assert get_walls(medium_empty_maze, 0, 0) == (False, False, True, True)
+    assert get_walls(medium_empty_maze, 2, 0) == (False, True, True, False)
+    assert get_walls(medium_empty_maze, 2, 2) == (True, True, False, False)
+    assert get_walls(medium_empty_maze, 0, 2) == (True, False, False, True)
+    assert get_walls(medium_empty_maze, 1, 1) == (False, False, False, False)
+
+    medium_maze = maze_reader("src/medium_maze.mz")
+    assert get_dimensions(medium_maze) == (3, 3)
+
+    # Maze looks like this:
+    # # # # # # #
+    # . . . # . #
+    # . # # # . #
+    # . . . . . #
+    # . # # # . #
+    # . # . . . #
+    # # # # # # #
+
+    assert get_walls(medium_maze, 0, 0) == (False, True, True, True)
+    assert get_walls(medium_maze, 1, 0) == (True, False, True, True)
+    assert get_walls(medium_maze, 2, 0) == (False, True, True, False)
+
+    assert get_walls(medium_maze, 0, 1) == (False, False, False, True)
+    assert get_walls(medium_maze, 1, 1) == (True, False, True, False)
+    assert get_walls(medium_maze, 2, 1) == (False, True, False, False)
+
+    assert get_walls(medium_maze, 0, 2) == (True, False, False, True)
+    assert get_walls(medium_maze, 1, 2) == (True, True, True, False)
+    assert get_walls(medium_maze, 2, 2) == (True, True, False, True)
